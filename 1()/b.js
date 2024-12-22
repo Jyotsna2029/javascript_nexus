33aeaafe019a5d1508f950a40638a3b85eeaa6db
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2024-12-22 07:58:07 +0530

    Add files via upload

---
+++ 1()/b.js
@@ -0,0 +1,15 @@
+// let
+let num = 10;
+console.log(num);
+
+let ar ="string";
+ar = "new string";
+console.log(ar);
+
+// const
+const ids = 20;
+console.log(ids);
+
+// var(old)
+var name = "name";
+console.log(name);
