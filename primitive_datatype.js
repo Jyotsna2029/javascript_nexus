77c1f10195f25d3636d4605555fea4f096a7a05f
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-01-01 03:35:03 +0530

    Add files via upload

---
+++ primitive_datatype.js
@@ -0,0 +1,35 @@
+// // declare the variable
+// let num = 10;
+// console.log(num);
+
+
+// // data types in JS
+//                         // primitive data type
+// // 1.Number
+// let num1 = 10;
+// //  2.String 
+// let str = "Hello";
+// let ab = 'jyotsna';
+// console.log(str);
+// console.log(ab);
+// //  3.Boolean 
+// let isTrue = true;
+// console.log(isTrue);
+// console.log(typeof isTrue); 
+// // 4.Undefined
+// let x;
+// console.log(x);
+// //  5.Null 
+// let y = null;
+// console.log(y);
+// console.log(typeof y);
+// // 6.Symbol
+// let sym = Symbol
+// console.log(sym);
+// //  7.BigInt
+// let bigInt = 12345678998765432345678765n;
+// console.log(bigInt);
+
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
